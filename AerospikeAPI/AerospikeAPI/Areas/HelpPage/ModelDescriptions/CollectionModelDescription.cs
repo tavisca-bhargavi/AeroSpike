e96f28c8f5456a859a589579b25605255d968a9e
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-09-29 16:31:03 +0530

    API

---
+++ AerospikeAPI/AerospikeAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AerospikeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AerospikeAPI/AerospikeAPI/Controllers/AerospikeController.cs
@@ -0,0 +1,29 @@
+﻿using Aerospike.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AerospikeAPI.Controllers
+{
+    public class AerospikeController : ApiController
+    {
+        public List<Record> GetAllData([FromUri]string [] tweetIds)
+        {
+            var aerospikeClient = new AerospikeClient("18.235.70.103", 3000);
+            string nameSpace = "AirEngine";
+            string setName = "Bhargavi";
+            List<Record> output = new List<Record>();
+            foreach (var id in tweetIds)
+            {
+              var key = new Key(nameSpace, setName, id);
+              Record tweetById = aerospikeClient.Get(new WritePolicy(), key);
+              output.Add(tweetById);
+            }
+            return output;
+
+        }
+    }
+}
